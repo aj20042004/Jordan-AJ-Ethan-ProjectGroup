e49ecde7d8f461d45f7b4b5bc51584b0c673d986
Athul Jaishankar ( AJ ) <athuljaishankar1@gmail.com>
2024-02-17 07:44:21 -0600

    Added the VS Code Project Folder

---
+++ Movie_Management_System/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+	// Test Cases goes here
+	
+	
+
+
+
+	system("pause");
+	return 0;
+}
